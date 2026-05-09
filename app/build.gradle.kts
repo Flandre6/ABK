002b105e1884860ea695e183d54347bafe7f8129
xingguangcuican6666 <xingguangcuican666@foxmail.com>
2026-05-10 00:49:30 +0800

    Use AGP built-in Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.serialization)
 }
 
 val githubClientId = providers.gradleProperty("ABK_GITHUB_CLIENT_ID")
@@ -71,9 +69,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
